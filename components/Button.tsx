576720bf575be161a38f4439b0f1dc35b057cffd
Konstantin Bartosh <rustigillo@gmail.com>
2023-12-05 18:53:46 +0600

    Feat: create Button component

---
+++ components/Button.tsx
@@ -0,0 +1,41 @@
+interface ButtonProps {
+  label: string;
+  secondary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  disabled?: boolean;
+  outline?: boolean;
+  onClick: () => void;
+}
+
+const Button = ({
+  label, secondary, fullWidth, large, disabled, outline, onClick
+}: ButtonProps ) => {
+  return(
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-full
+        font-semibold
+        hover:opacity-80
+        border-2
+        ${fullWidth ? 'w-full' : 'w-fit'}
+        ${secondary ? 'bg-white' : 'bg-sky-500'}
+        ${secondary ? 'text-black' : 'text-white'}
+        ${secondary ? 'border-black' : 'border-sky-500'}
+        ${large ? 'text-xl' : 'text-base'}
+        ${large ? 'p-5' : 'p-4'}
+        ${outline ? 'bg-transparent' : ''}
+        ${outline ? 'border-white' : ''}
+        ${outline ? 'text-white' : ''}
+      `}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button;
--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
   ]
 
   return(
-    <nav className='col-span-1 h-full pr-4 md:pr-6'>
+    <nav className='col-span-1 h-full pr-4 md:pr-6 pt-4'>
       <div className='flex flex-col items-end'>
         <div className='space-y-2 lg:w-[230px]'>
           <SidebarLogo />
