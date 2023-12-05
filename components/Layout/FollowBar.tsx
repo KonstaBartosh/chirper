a41a1fa2d58e1e10e1b20dbf3556a2c8fde1907a
Konstantin Bartosh <rustigillo@gmail.com>
2023-12-05 18:28:28 +0600

    Feat: create  all layouts & made it responsive

---
+++ components/Layout/FollowBar.tsx
@@ -0,0 +1,16 @@
+const FollowBar = () => {
+  return(
+    <aside className="px-6 py-4 hidden lg:block">
+      <div className=" bg-neutral-800 p-4 rounded-xl">
+        <h2 className="text-white text-xl font-semibold">
+          Who to follow
+        </h2>
+        <div className="mt-4 flex flex-col gap-6">
+          {/** TODO user list */}
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default FollowBar;
