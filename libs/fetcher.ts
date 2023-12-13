741edc1a6f68caa74047df49d2df760cd66701ad
Konstantin Bartosh <rustigillo@gmail.com>
2023-12-13 19:36:15 +0600

    Fix: forget to return fetch request

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const fetcher = (url: string) => {
-  axios
-    .get(url)
-    .then((res) => res.data);
+  return axios.get(url).then((res) => res.data);
 }
 
 export default fetcher;
