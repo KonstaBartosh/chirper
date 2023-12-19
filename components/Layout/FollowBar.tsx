6ed406956b3ed2404719077beb83aacce9217e3c
Konstantin Bartosh <rustigillo@gmail.com>
2023-12-19 19:13:07 +0600

    Refactor: markup

--- components/Layout/FollowBar.tsx
+++ components/Layout/FollowBar.tsx
@@ -1,12 +1,29 @@
+import useUsers from "@/hooks/useUsers";
+
+import Avatar from "../Avatar";
+
 const FollowBar = () => {
+  const { data: users = []} = useUsers();
+  console.log(users)
+
+  if (users.length === 0) {
+    return null;
+  }
+
   return(
-    <aside className="px-6 py-4 hidden lg:block">
+    <aside className="px-6 py-4 hidden lg:block bg-red-950">
       <div className=" bg-neutral-800 p-4 rounded-xl">
-        <h2 className="text-white text-xl font-semibold">
-          Who to follow
-        </h2>
+        <h2 className="text-white text-xl font-semibold">Who to follow</h2>
         <div className="mt-4 flex flex-col gap-6">
-          {/** TODO user list */}
+          {users.map((user: any) => (
+            <div key={user.id} className="flex flex-row gap-4">
+              <Avatar userId={user.id}/>
+              <div className="flex flex-col">
+                <p className="text-sm text-white font-semibold">{user.name}</p>
+                <p className="text-neutral-400 text-sm">@{user.username}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </aside>
