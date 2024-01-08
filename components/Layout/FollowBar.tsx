db5aa6d1fe1ed1c3835909eaa30e0c488ba57c01
Konstantin Bartosh <rustigillo@gmail.com>
2024-01-08 17:11:15 +0300

    Refactor: add russian localization

--- components/Layout/FollowBar.tsx
+++ components/Layout/FollowBar.tsx
@@ -13,7 +13,7 @@ const FollowBar = () => {
   return(
     <aside className="px-6 py-4 hidden lg:block">
       <div className=" bg-neutral-800 p-4 rounded-xl">
-        <h2 className="text-white text-xl font-semibold">Who to follow</h2>
+        <h2 className="text-white text-xl font-semibold">На кого подписаться</h2>
         <div className="mt-4 flex flex-col gap-6">
           {users.map((user: any) => (
             <div key={user.id} className="flex flex-row gap-4">
